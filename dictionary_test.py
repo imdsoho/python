37cef20147bd4dcae2895a40978c5e37051c4d65
imdsoho <imdsoho@gmail.com>
2019-03-07 00:00:27 +0900

    test code commit

--- dictionary_test.py
+++ dictionary_test.py
@@ -12,4 +12,26 @@ b = {
 
 c = { key:a[key] for key in a.keys() - {'x', 'w'}}
 
-print (c)
+#print (c)
+
+def dedupe(items, key=None):
+    seen = set()
+
+    #print (type(key))
+
+    for item in items:
+        print ("1", item, key(item))
+
+        val = item if key is None else key(item)
+
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+alist = [{'x':1, 'y':2}, {'x':1,'y':3}, {'x':1, 'y':2}, {'x':2,'y':4}]
+
+seen = list(dedupe(alist, key=lambda d: (d['x'], d['y'])))
+
+print (seen)
+
+
