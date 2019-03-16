4f0fa4f5e7ec4d47027c45d686263e3b68c6c476
imdsoho <imdsoho@gmail.com>
2019-03-16 18:53:03 +0900

    chainMap_namedtuple_slots

---
+++ DataStructure/chainMap.py
@@ -0,0 +1,22 @@
+# ChainMap은 매핑을 여러 개 받아서 하나처럼 보이게 한다.
+# 매핑에 대한 리스트를 유지하면서 리스트를 스캔하도록 일반적인 딕셔너리 동작을 재정의
+# 중복키가 있으면 첫번째 매핑의 값을 사용
+
+from collections import ChainMap
+a = {'x':1, 'z':2}
+b = {'y':2, 'z':3}
+c = ChainMap(a, b)
+print (len(c)) # 3
+
+values = ChainMap()
+values['x'] = 1
+values = values.new_child()
+values['x'] = 2
+
+# 완전히 별개의 딕셔너리를 생성하여 하나로 합친다.
+# 원본 딕셔너리의 내용을 변경해도 합쳐 놓은 딕셔너리에 영향은 없다.
+merged = dict(a)
+merged.update(a)
+print (len(merged)) # 3
+
+# ChainMap은 원본 딕셔너리를 참조한다. 원본 딕셔너리의 변경을 반영한다.
---
+++ DataStructure/namedtuple.py
@@ -0,0 +1,31 @@
+# collections.namedtuple()은 실제로 표준 파이썬 tuple 타입의 서브클래스를 반환하는 팩토리 메소드이다.
+# 타입 이름과 포함해야 할 필드를 전달하면 인스턴스화 가능한 클래스를 반환
+
+from collections import namedtuple
+
+Subscriber = namedtuple('Subscriber', ['addr', 'joined'])
+sub = Subscriber('abc@example.com', '2012-10-19')
+print (type(sub))
+
+Stock = namedtuple('Stock', ['name', 'shares', 'price'])
+def compute_cost(records):
+    total = 0.0
+    for rec in records:
+        s = Stock(*rec)
+        total += s.shares * s.price
+    return total
+
+# namedtuple은 수정할 수 없다.
+# 속성을 수정하려면 namedtuple 인스턴스의 _replace() 메소드를 사용해야 한다.
+# 값을 치환하여 새로운 namedtuple을 생성한다.
+
+s = Stock('ACME', 100, 123.45)
+s = s._replace(shares=75)
+
+# 프로토타입 튜플 생성
+Stock = namedtuple('Stock', ['name', 'shares', 'price', 'date', 'time'])
+stock_prototyp = Stock('', 0, 0.0, None, None)
+def dict_to_stock(s):
+    return stock_prototyp._replace(**s)
+
+
