872cc49ed5856203a6b9bf538dff1886f1cd9245
imdsoho <imdsoho@gmail.com>
2019-03-28 17:13:17 +0900

    pojo function test

---
+++ utility/pojo.py
@@ -0,0 +1,8 @@
+a = 'hello'
+b = 'my name'
+c = 'imdsoho'
+print(a, b, c, sep=':')
+
+# print(*objects, sep=' ', end='\n', file=sys.stdout)
+# Print objects to the stream file, separated by sep and followed by end.
+# sep, end and file, if present, must be given as keyword arguments.
