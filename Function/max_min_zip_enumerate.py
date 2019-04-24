4fb92918f8717b737a9889446701c2d31eefe540
jsc <jsc@intellicon.co.kr>
2019-04-24 13:35:40 +0900

    enumerate, itertools

--- Function/max_min_zip_enumerate.py
+++ Function/max_min_zip_enumerate.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 # zip() 은 단 한 번만 소비할 수 있는 iterator를 생성한다. 동일한 갯수의 요소값을 갖는 시퀀스 자료형을 묶어주는 역할
 
 zip([1,2,3], ['a','b'])
@@ -19,3 +21,30 @@ min(zip(prices.values(), prices.keys()))
 
 max(zip(prices.values(), prices.keys()))
 # (612.78, 'AAPL')
+
+# enumerate()
+my_list = ['a', 'b', 'c']
+for idx, val in enumerate(my_list, 1):
+    print(idx, val)
+
+def parse_data(file_name):
+    with open(filename, 'rt') as f:
+        for lineno, line in enumerate(f, 1):
+            fields = line.split()
+
+            try:
+                count = int(fields[1])
+            except:
+                print('Line {}: Parse error: {}'.format(lineno, e))
+
+word_summary = defaultdict(list)
+
+with open('myflle.txt', 'r') as f:
+    lines = f.readlines()
+
+for idx, line in enumerate(lines):
+    words = [w.strip().lower() for w in line.split()]
+
+    for word in words:
+        word_summary[word].append(idx)
+
---
+++ Module/pyItertools.py
@@ -0,0 +1,26 @@
+from itertools import islice, dropwhile, permutations, combinations, combinations_with_replacement
+
+def count(n):
+    while True:
+        yield n
+        n += 1
+
+c = count(5)
+
+for x in itertools.islice(c, 10, 20):
+    print(x)
+
+# islice()
+# 순환 객체의 앞 부분 자르기
+
+# dropwhile()
+# 순환 객체의 내용 중 일정한 조건에 맞는 내용을 생략하기
+
+# permutations()
+# 아이템 컬렉션을 받아 가능한 모든 순열을 튜플 시퀀스로 생성
+
+# combinations()
+# 입력 받은 아이템의 가능한 조합 생성
+
+# combinations_with_replacement()
+# 중복을 허용하여 가능한 조합 생성
