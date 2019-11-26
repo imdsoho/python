37d24331c4f1d2e34cd6e4eec5a07962ccaf63ee
jung sung chul <jsc@intellicon.co.kr>
2019-11-26 18:14:04 +0900

    테스트 코드 추가

--- SWArchitectureTest/main.py
+++ SWArchitectureTest/main.py
@@ -5,8 +5,5 @@ from instance_factory import getInstance
 es_data = getInstance(DataObjectESServiceImpl)
 print(es_data.get_data())
 
-db_data = getInstance(DataObjectDBServiceImpl)
-print(db_data.get_data())
-
-
-
+#db_data = getInstance(DataObjectDBServiceImpl)
+#print(db_data.get_data())
