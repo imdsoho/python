cbf9f25840706a3d97f5b5dcd394a8c7c221bdbe
jung sung chul <jsc@intellicon.co.kr>
2019-12-23 18:26:49 +0900

    reference count test 코드 추가

---
+++ memory_gc/reference_count.py
@@ -0,0 +1,37 @@
+import sys
+
+class ClassObject:
+    pass
+
+def functionObject_1():
+    pass
+
+def functionObject_2():
+    pass
+
+def show_ref_count():
+    str_obj = "string_data"
+    print("str : ", sys.getrefcount(str_obj))
+
+    dict_obj = {"a":"1", "b":2}
+    print("dict : ", sys.getrefcount(dict_obj))
+
+    tuple_obj = (1,2,3,"a","b")
+    print("tuple : ", sys.getrefcount(tuple_obj))
+
+    set_obj = {"a", "b", "c"}
+    print("set : ", sys.getrefcount(set_obj))
+
+    arr_obj = [1,2,3]
+    print("arrary : ", sys.getrefcount(arr_obj))
+
+    cls_obj = ClassObject()
+    print("class : ", sys.getrefcount(cls_obj))
+
+    print("func-1 : ", sys.getrefcount(functionObject_1))
+
+    func_obj = functionObject_2()
+    print("func-2 : ", sys.getrefcount(func_obj))
+
+
+show_ref_count()
